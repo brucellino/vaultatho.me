775f4b86673d84dced1797ad77655175d11c6213
Bruce Becker <brucellino@protonmail.com>
2022-05-14 10:08:39 +0200

    feat(policy): add admin monitoring policy
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- policies.tf
+++ policies.tf
@@ -1,19 +1,15 @@
-data "local_file" "read_only_policy" {
-  filename = "${path.module}/policies/read-only.hcl"
-}
-
 resource "vault_policy" "read_only" {
   name   = "read-only"
-  policy = data.local_file.read_only_policy.content
-}
-
-# read Nomad monitoring policy from local file
-data "local_file" "monitoring_policy" {
-  filename = "${path.module}/policies/nomad-monitoring.hcl"
+  policy = file("${path.module}/policies/read-only.hcl")
 }
 
 # Create Vault policy from nomad monitoring data
 resource "vault_policy" "nomad_monitoring" {
   name   = "nomad-monitoring"
-  policy = data.local_file.monitoring_policy.content
+  policy = file("${path.module}/policies/nomad-monitoring.hcl")
+}
+
+resource "vault_policy" "admin" {
+  name   = "admin"
+  policy = file("${path.module}/policies/admin.hcl")
 }
