7f8f229965d0497356f19eec88f201ac5269e334
Bruce Becker <brucellino@protonmail.com>
2022-11-25 14:42:01 +0100

    chore: add a default consul http address
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- variables.tf
+++ variables.tf
@@ -3,3 +3,10 @@ variable "vault_addr" {
   description = "The address of the Vault server"
   sensitive   = true
 }
+
+variable "consul_http_addr" {
+  type        = string
+  description = "URL of the Consul server endpoint"
+  sensitive   = false
+  default     = "http://bare:8500"
+}
