b99105b1d7dccd8cc39ff55494e983b01ba68c6a
Bruce Becker <brucellino@protonmail.com>
2023-11-05 10:30:14 +0100

    fix(aws): remove aws secrets engine
    BREAKING CHANGE: This removes the AWS secrets engine and all data in it
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- aws.tf
+++ aws.tf
@@ -2,24 +2,24 @@
 # We only add the role here, since we don't want to add the
 # secrets in this file
 
-resource "vault_aws_secret_backend_role" "full_access" {
-  backend         = "acg"
-  name            = "full-access"
-  credential_type = "iam_user"
-  policy_document = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowAllPermissions",
-      "Effect": "Allow",
-      "NotAction": [
-        "lightsail:*",
-        "sagemaker:*"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOT
-}
+# resource "vault_aws_secret_backend_role" "full_access" {
+#   backend         = "acg"
+#   name            = "full-access"
+#   credential_type = "iam_user"
+#   policy_document = <<EOT
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "AllowAllPermissions",
+#       "Effect": "Allow",
+#       "NotAction": [
+#         "lightsail:*",
+#         "sagemaker:*"
+#       ],
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOT
+# }
