f1213160b77ec533ef7f51ac9fa48a5cd807247b
Bruce Becker <brucellino@protonmail.com>
2024-06-30 12:52:24 +0200

    feat(nomad): add nomad secrets mount (#118)
     ci: add nomad to matrix and bootstrap acls
     ci: fix call to nomad version
     ci: overwrite files when unzipping
    ci: use relative path to consul and nomad
    ci: don't bootstrap acl for now
    ci: extract nomad token from bootstrap json
    feat(nomad): add secrets backend
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>
    
    * fix(consul): add ttl to consul tokens
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>
    
    * ci: fix name of nomad token var
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>
    
    ---------
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- consul.tf
+++ consul.tf
@@ -1,13 +1,17 @@
 resource "vault_consul_secret_backend" "consul" {
-  path        = var.consul_backend_path
-  address     = var.consul_http_addr
-  description = "Consul Tokens Path"
-  bootstrap   = false
-  token       = var.consul_token
+  path                      = var.consul_backend_path
+  address                   = var.consul_http_addr
+  description               = "Consul Tokens Path"
+  bootstrap                 = false
+  token                     = var.consul_token
+  default_lease_ttl_seconds = 300
+  max_lease_ttl_seconds     = 600
 }
 
 resource "vault_consul_secret_backend_role" "cluster" {
   name            = "cluster-role"
   backend         = vault_consul_secret_backend.consul.path
-  consul_policies = ["global-management"]
+  consul_policies = ["backup-policy"]
+  ttl             = 60
+  max_ttl         = 300
 }
